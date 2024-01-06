308531e18c3da2e26a70079fe982d169c9925770
hiro5900 <achyut5900@gmail.com>
2024-01-06 23:09:16 +0530

    Created an overlay that will show up when page loads

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -33,6 +33,7 @@ const DUMMY_MEALS = [
 const AvailableMeals = () => {
      const mealsList = DUMMY_MEALS.map(meal => (
           <MealItem
+               id={meal.id}
                key={meal.id}
                name={meal.name}
                description={meal.description}
@@ -40,7 +41,6 @@ const AvailableMeals = () => {
           />
      ))
      return (
-
           <section className={classes.meals}>
                <Card>
                     <ul>

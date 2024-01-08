f863ce68a4df352ebb7f18b9ed249e7268c9cf57
hiro5900 <achyut5900@gmail.com>
2024-01-09 00:49:11 +0530

    Cart can now be closed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import React, { useState } from 'react';
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 
 function App() {
+  const [cartIsShown, setIsCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setIsCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setIsCartIsShown(false);
+  };
   return (
     <>
-      <Cart />
-      <Header />
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>

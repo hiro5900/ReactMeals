e7e43099ed84e3154c9954e09270790e1e79c3eb
hiro5900 <achyut5900@gmail.com>
2024-01-28 00:53:58 +0530

    Cart items' quantity can be increased/decreased but re-rendering is required

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,15 +1,30 @@
-import React, {useContext} from 'react'
+import React, {useContext, useEffect} from 'react'
 import classes from './Cart.module.css'
 import Modal from '../UI/Modal'
 import Cart_Context from '../../store/cart-context'
 
 const Cart = (props) => {
      const cart_Ctx = useContext(Cart_Context)
+     useEffect(() => {
+          console.log(cart_Ctx.items.quantity)
+     }, [cart_Ctx.items.quantity])
+
      const cartItems = (<ul className={classes['cart-items']}>
           {cart_Ctx.items.map((item) => (
-               <li>{item.name} ${item.price} Qty: {item.quantity}</li>
+               <div className={classes.cartItemContainer} key={Math.random()}>
+                    <div className={classes.cartItemMeal}>
+                         <h3>{item.name}</h3>
+                         <span className={classes.cartItemPrice}>${item.price}</span> 
+                         <span className={classes.cartItemQty}>x {item.quantity}</span> 
+                    </div>
+                    <div className={classes.cartItemButtons}>
+                         <button onClick={() => item.quantity--}>-</button>
+                         <button onClick={() => item.quantity++}>+</button>
+                    </div>
+               </div>
           ))}
      </ul>)
+
      let totalAmount = 0
      cart_Ctx.items.forEach((item) => {
           totalAmount += item.price * item.quantity
